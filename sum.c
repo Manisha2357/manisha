5bf3baa25a243d23167c117bb052122d2ea6936d
Manisha2357 <9923103108@mail.jiit.ac.in>
2025-07-30 11:22:58 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 int main() {
-
-    int number1, number2, sum;
-
-    printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
-
-    // calculate the sum
-    sum = number1 + number2;
-
-    printf("%d + %d = %d", number1, number2, sum);
+    int number, reversedNumber, digit1, digit2, digit3;
+    printf("Enter a three-digit number: ");
+    scanf("%d", &number);
+    digit1 = number % 10;        // Last digit
+    digit2 = (number / 10) % 10; // Middle digit
+    digit3 = number / 100;       // First digit
+    reversedNumber = digit1 * 100 + digit2 * 10 + digit3;
+    printf("Reversed number is: %d\n", reversedNumber);
     return 0;
 }
